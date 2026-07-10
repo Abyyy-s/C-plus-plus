4f5b103a54522dcbd84cc159bda216bdd0d0162e
Aby S Biju <abisbiju321@gmail.com>
2026-07-11 00:20:13 +0530

    updated

--- times.cpp
+++ times.cpp
@@ -1,2 +1,16 @@
 #include<iostream>
-using namespace std int main
+using namespace std;
+int main()
+ {
+ int arr[]={5,2,7,2,9,2};
+ int flag=0,i,t=2;
+ int n=sizeof(arr)/sizeof(arr[0]);
+ for(i=0;i<n;i++)
+ {
+ 	if(arr[i]==t)
+ 	{
+ 		flag++;}}
+ 		
+ cout<<"found "<<flag<<" times"<<endl;
+ return 0;
+ }

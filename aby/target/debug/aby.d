f07618761c479a8ef9cd0e1f4d28af2c7887a789
Aby S Biju <abisbiju321@gmail.com>
2026-07-15 23:35:12 +0530

    runned 1st rust pgm

---
+++ aby/target/debug/aby.d
@@ -0,0 +1,1 @@
+/home/aby/Documents/pgm/aby/target/debug/aby: /home/aby/Documents/pgm/aby/src/main.rs
---
+++ aby/target/debug/deps/aby-5b0dc04088e90a80.d
@@ -0,0 +1,5 @@
+/home/aby/Documents/pgm/aby/target/debug/deps/aby-5b0dc04088e90a80.d: src/main.rs
+
+/home/aby/Documents/pgm/aby/target/debug/deps/aby-5b0dc04088e90a80: src/main.rs
+
+src/main.rs:
